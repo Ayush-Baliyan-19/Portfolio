b378766718f9399f0de8123bab52a82fab04b369
ayushb_groww <ayushb.groww.in>
2025-01-30 23:04:30 +0530

    Add widgets to csp

--- csp.ts
+++ csp.ts
@@ -3,7 +3,7 @@ export const cspHeader = `
     default-src 'self';
   object-src 'none';
   block-all-mixed-content;
-  script-src 'self' 'nonce-${nonce}' https://*.webengage.com/ https://ayush-baliyan.tech/;
+  script-src 'self' 'nonce-${nonce}' https://ssl.widgets.webengage.com/ https://*.webengage.com/ https://ayush-baliyan.tech/;
   img-src 'self' data: blob: https://*.webengage.com/ https://ayush-baliyan.tech/;
   style-src 'self' 'unsafe-inline' https://*.webengage.com/ https://ayush-baliyan.tech/;
   font-src 'self' data: https://ayush-baliyan.tech/;
