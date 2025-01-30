aba6a386fdbc06ccc42bd55cb31feb29f525fb46
ayushb_groww <ayushb.groww.in>
2025-01-30 23:22:48 +0530

    Add www

--- csp.ts
+++ csp.ts
@@ -1,13 +1,13 @@
 export const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 export const cspHeader = `
-    default-src 'self';
+  default-src 'self';
   object-src 'none';
   block-all-mixed-content;
-  script-src 'self' 'nonce-${nonce}' https://ssl.widgets.webengage.com/ https://*.webengage.com/ https://ayush-baliyan.tech/;
-  img-src 'self' data: blob: https://*.webengage.com/ https://ayush-baliyan.tech/;
-  style-src 'self' 'unsafe-inline' https://*.webengage.com/ https://ayush-baliyan.tech/;
-  font-src 'self' data: https://ayush-baliyan.tech/;
-  connect-src 'self' data: https://*.webengage.com/ https://ayush-baliyan.tech/;
-  frame-src 'self' https://*.webengage.com/ https://ayush-baliyan.tech/;
-  media-src blob: https://ayush-baliyan.tech/;
+  script-src 'self' 'nonce-${nonce}' https://ssl.widgets.webengage.com/ https://*.webengage.com/ https://ayush-baliyan.tech/ https://www.ayush-baliyan.tech/;
+  img-src 'self' data: blob: https://*.webengage.com/ https://ayush-baliyan.tech/ https://www.ayush-baliyan.tech/;
+  style-src 'self' 'unsafe-inline' https://*.webengage.com/ https://ayush-baliyan.tech/ https://www.ayush-baliyan.tech/;
+  font-src 'self' data: https://ayush-baliyan.tech/ https://www.ayush-baliyan.tech/;
+  connect-src 'self' data: https://*.webengage.com/ https://ayush-baliyan.tech/ https://www.ayush-baliyan.tech/;
+  frame-src 'self' https://*.webengage.com/ https://ayush-baliyan.tech/ https://www.ayush-baliyan.tech/;
+  media-src blob: https://ayush-baliyan.tech/ https://www.ayush-baliyan.tech/;
 `;
